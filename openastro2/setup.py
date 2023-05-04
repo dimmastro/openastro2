a3cb3248c64e63d1e644644755f1bb8f4258a74a
dimmastro <dmitriy.koriskiy@esynergy.lv>
2023-05-04 23:50:25 +0500

    OpenAstro2 setup.py

--- openastro2/setup.py
+++ openastro2/setup.py
@@ -1,34 +1,14 @@
-#!/usr/bin/env python3
+from setuptools import setup
 
-from distutils.core import setup, Extension
-import glob, os.path, sys
-
-f=open('VERSION')
-VERSION=f.read()
-f.close()
-
-f=glob.glob('locale/*')
-pre_data_files=[]
-for a in range(len(f)):
-	if f[a] == "locale/templates":
-		continue
-	pre_data_files.append( ('share/openastro.org/%s/LC_MESSAGES'%(f[a]),['%s/LC_MESSAGES/openastro.mo'%(f[a])]) )
-
-pre_data_files += [
-	('share/applications', ['openastro.desktop']),
-	('share/openastro.org', ['openastro-svg.xml','openastro-svg-table.xml','openastro-ui.xml']),
-	('share/openastro.org/icons', ['icons/openastro.svg']),
-	('share/openastro.org/icons/aspects', glob.glob('icons/aspects/*.svg'))
-	]
-
-setup(name='OpenAstro.org',
-      version=VERSION,
-      description='Open Source Astrology',
-      author='Pelle van der Scheer',
-      author_email='devel@openastro.org',
-      url='http://www.openastro.org',
+setup(name='OpenAstro2',
+      version=2.0.0,
+      description='Open Source Astrology 2',
+      author='Pelle van der Scheer + DimmAstro',
+      author_email='',
+      url='https://github.com/dimmastro/openastro2',
+      packages=["openastro2"],
+      package_dir={"openastro2": "openastro2"},
       license='GPL',
-      scripts=['openastro'],
+      scripts=['openastro2'],
       packages=['openastromod'],
-      data_files=pre_data_files
      )

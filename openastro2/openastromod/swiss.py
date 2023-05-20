f0f3e5ccad05a25b88653d8177fcd34a3f19d2aa
dimmastro <dmitriy.koriskiy@esynergy.lv>
2023-05-21 00:23:15 +0500

    OpenAstro2 Add hour_angle

--- openastro2/openastromod/swiss.py
+++ openastro2/openastromod/swiss.py
@@ -109,7 +109,7 @@ class ephData:
 						self.planet_longitude[i] = ret_flag[0][0]
 						self.planet_latitude[i] = ret_flag[0][1]
 						# Вычислите часовой угол планеты в градусах
-						self.planet_hour_angle[i] = swe.degnorm(swe.sidtime(self.jul_day_UT) - self.planet_longitude[i] - geolon -9)
+						self.planet_hour_angle[i] = swe.degnorm(swe.sidtime(self.jul_day_UT) - self.planet_longitude[i] - geolon -12)
 						print ('self.jul_day_UT')
 						print (self.jul_day_UT)
 						print ('swe.sidtime(self.jul_day_UT)')

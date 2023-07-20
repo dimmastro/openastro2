b89bb9633a2f61f78c454cacceba2e956c207304
dimmastro <dmitriy.koriskiy@esynergy.lv>
2023-07-20 18:08:18 +0500

    OpenAstro2 Add ZenitLayer 4

--- openastro2/openastro2.py
+++ openastro2/openastro2.py
@@ -3248,7 +3248,7 @@ class openAstro:
 				  "to": {
 					"name": self.name + "/ " + planet_names[i],
 					"coordinates": [
-					  179,
+					  90,
 					  planet_subpoint.latitude.degrees
 					]
 				  }
